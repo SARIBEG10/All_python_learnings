5b6d3d0437de4a94dd7138d6aa22daac12c79eca
SARIBEG10 <77942266+SARIBEG10@users.noreply.github.com>
2021-04-08 18:54:17 +0430

    Update 3.py

--- SMTP(sending email)/3.py
+++ SMTP(sending email)/3.py
@@ -7,8 +7,8 @@ import emoji
 
 host = 'smtp.gmail.com'
 port = 587
-username = 'sfarmanian@gmail.com'
-password = 'SariBeg-137216-Farmanian'
+username = 'email'
+password = 'pass'
 
 connection = smtp.SMTP(host, port)
 connection.ehlo()
